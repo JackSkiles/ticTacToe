d95bc8512b33912a1a7c9f23b4d9db3b8d62abea
Jack Skiles <jackskiles420@gmail.com>
2020-08-29 16:04:29 -0400

    created tokens array for game board

--- ticTacToe/Board.java
+++ ticTacToe/Board.java
@@ -1,5 +1,28 @@
 package ticTacToe;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Board {
-
+	public static void main(String[] args) {
+			
+		Scanner user = new Scanner(System.in);
+		
+		ArrayList<CpuPlayer> gameList = new ArrayList();
+		ArrayList gameBoard = new ArrayList(6);
+		
+		for(int i = 0; i < 9; i++) {
+			gameBoard.add(i);
+		}
+		
+		gameBoard.set(0, "X");
+		gameBoard.set(5, "O");
+		
+		System.out.print(gameBoard.get(0));
+		
+		boolean game = true;
+		
+		while(game == true) {
+			
+		}
+	}
 }
--- ticTacToe/CpuPlayer.java
+++ ticTacToe/CpuPlayer.java
@@ -1,5 +1,8 @@
 package ticTacToe;
 
 public class CpuPlayer {
-
+	String cpuName;
+	int losingSpace1;
+	int losingSpace2;
+	int losingSpace3;
 }
