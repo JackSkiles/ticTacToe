e4cebff016e7f9c64c2eee3c691237e967f06cf5
Jack Skiles <jackskiles420@gmail.com>
2020-08-31 13:14:01 -0400

    Finished working 2 player game. Next is cpu player

--- ticTacToe/Board.java
+++ ticTacToe/Board.java
@@ -28,6 +28,9 @@ public class Board {
 		
 		String menu1 = user.nextLine();
 		
+		boolean player = true;
+		String playerToken = "X";
+		
 		switch(menu1) {
 			case "1" :
 				while(game == true) {
@@ -36,18 +39,60 @@ public class Board {
 					System.out.println("| " + gameBoard.get(3) + " | " + gameBoard.get(4) + " | " + gameBoard.get(5) + " |");
 					System.out.println("| " + gameBoard.get(6) + " | " + gameBoard.get(7) + " | " + gameBoard.get(8) + " |");
 					System.out.println(" ----------- ");
-					String tokenChoice = user.nextLine();
-					int token = Integer.parseInt(tokenChoice);
-					gameBoard.set(token, "X");
-					if(gameBoard.get(0) == "X" && gameBoard.get(1) == "X" && gameBoard.get(2) == "X") {
+					if (player == true) {
+						System.out.println("Player1 turn");
+						playerToken = "X";
+						String squareChoice = user.nextLine();
+						int token = Integer.parseInt(squareChoice);
+						gameBoard.set(token, "X");
+						player = false;
+					} else if (player == false) {
+						System.out.println("Player2 turn");
+						playerToken = "O";
+						String squareChoice = user.nextLine();
+						int token = Integer.parseInt(squareChoice);
+						gameBoard.set(token, "O");
+						player = true;
+					}
+					if(gameBoard.get(0) == playerToken && gameBoard.get(1) == playerToken && gameBoard.get(2) == playerToken) {
+						System.out.println("X wins");
+						break;
+					} else if(gameBoard.get(0) == playerToken && gameBoard.get(3) == playerToken && gameBoard.get(6) == playerToken) {
+						System.out.println("X wins");
+						break;
+					} else if(gameBoard.get(0) == playerToken && gameBoard.get(4) == playerToken && gameBoard.get(8) == playerToken) {
+						System.out.println("X wins");
+						break;
+					} else if(gameBoard.get(1) == playerToken && gameBoard.get(4) == playerToken && gameBoard.get(7) == playerToken) {
+						System.out.println("X wins");
+						break;
+					} else if(gameBoard.get(2) == playerToken && gameBoard.get(5) == playerToken && gameBoard.get(8) == playerToken) {
+						System.out.println("X wins");
+						break;
+					} else if(gameBoard.get(2) == playerToken && gameBoard.get(4) == playerToken && gameBoard.get(6) == playerToken) {
+						System.out.println("X wins");
+						break;
+					} else if(gameBoard.get(0) == playerToken && gameBoard.get(3) == playerToken && gameBoard.get(6) == playerToken) {
+						System.out.println("X wins");
+						break;
+					} else if(gameBoard.get(3) == playerToken && gameBoard.get(4) == playerToken && gameBoard.get(5) == playerToken) {
+						System.out.println("X wins");
+						break;
+					} else if(gameBoard.get(6) == playerToken && gameBoard.get(7) == playerToken && gameBoard.get(8) == playerToken) {
 						System.out.println("X wins");
 						break;
 					}
+
 				}
 			case "2" : 
 				System.out.println("Thank you for playing!");
 				break;
 		}
+		System.out.println(" ----------- ");
+		System.out.println("| " + gameBoard.get(0) + " | " + gameBoard.get(1) + " | " + gameBoard.get(2) + " |");
+		System.out.println("| " + gameBoard.get(3) + " | " + gameBoard.get(4) + " | " + gameBoard.get(5) + " |");
+		System.out.println("| " + gameBoard.get(6) + " | " + gameBoard.get(7) + " | " + gameBoard.get(8) + " |");
+		System.out.println(" ----------- ");
 //		while(game == true) {
 //			System.out.println(" -------- ");
 //			System.out.println("| " + gameBoard.get(0) + " | " + gameBoard.get(1) + " | " + gameBoard.get(2) + " |");
